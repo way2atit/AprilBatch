e39fb60e9aafa398cc5fb658c3a27bcf55328a32
way2atit <way2atit@gmail.com>
2023-08-31 18:07:12 +0530

    employee file changed

--- src/test/java/com/july/Employee.java
+++ src/test/java/com/july/Employee.java
@@ -3,6 +3,14 @@ package com.july;
 public class Employee {
 	
 	int id;
+	String name;
 	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		 Employee e1 = new Employee();
+	}
 
 }
