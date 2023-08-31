5ea4e8cb984858d249850976ddf63663e3805586
way2atit <way2atit@gmail.com>
2023-08-31 18:31:46 +0530

    address

--- src/test/java/com/java/AprilBatch23/Address.java
+++ src/test/java/com/java/AprilBatch23/Address.java
@@ -4,6 +4,18 @@ public class Address {
 
 	public void m1()
 	{
-		
+		System.out.println("from github");
+		System.out.println("m1");
+		System.out.println("m1");
+		System.out.println("m1");
+		System.out.println("m1");
+		System.out.println("m1");
+		System.out.println("m1");
+		System.out.println("m1");
+	
+	}
+	public void m2()
+	{
+		System.out.println("m1");
 	}
 }
