237fa822f0fb31fd980d77c48bdd4ce790d39d9d
way2atit <97036773+way2atit@users.noreply.github.com>
2023-08-31 18:27:41 +0530

    Update Address.java

--- src/test/java/com/java/AprilBatch23/Address.java
+++ src/test/java/com/java/AprilBatch23/Address.java
@@ -4,6 +4,9 @@ public class Address {
 
 	public void m1()
 	{
-		
+		System.out.println("from github");
+		System.out.println("m1");
+		System.out.println("m1");
+		System.out.println("m1");
 	}
 }
