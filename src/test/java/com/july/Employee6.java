c5fd2b0fedfa366ae342e70a8e7257a71468af7f
way2atit <way2atit@gmail.com>
2023-08-31 18:14:45 +0530

    commited

---
+++ src/test/java/com/july/Employee6.java
@@ -0,0 +1,17 @@
+package com.july;
+
+public class Employee6 {
+	
+	int id;
+	String name;
+	String city;
+	
+	public Employee6() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		 Employee6 e1 = new Employee6();
+	}
+
+}
